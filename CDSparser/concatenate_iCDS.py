dc880e22f1515a05a4f727e2e949fe90099f6e99
haiwangyang <haiwangyang@gmail.com>
2018-05-27 09:48:22 -0400

    m

--- CDSparser/concatenate_iCDS.py
+++ CDSparser/concatenate_iCDS.py
@@ -13,6 +13,17 @@ output data/PEP.fa
 """
 
 from pyfaidx import Fasta
+from Bio.Seq import Seq
+
+def write_dct_fasta(dct, path):
+    with open(path, "w") as f:
+        for i in dct.keys():
+            f.write(">" + i + "\n" + dct[i] + "\n")
+
+def write_dct_table(dct, path):
+    with open(path, "w") as f:
+        for i in dct.keys():
+            f.write(i + "\t" + dct[i] + "\n")
 
 class CDS:
     """ CDS object """
@@ -20,8 +31,9 @@ class CDS:
         self.iCDSfasta = Fasta("data/iCDS.fa", as_raw = True)
         self.get_iCDS_dct()
         self.get_CDS_dct()
-        #self.check_stop_codon()
+        self.check_stop_codon()
         self.check_3n()
+        self.get_PEP_dct()
 
     def get_iCDS_dct(self):
         """ get iCDS dict """
@@ -56,20 +68,36 @@ class CDS:
         self.CDSdct = dct
 
     def check_stop_codon(self):
+        dct = dict()
         for geneid in self.CDSdct.keys():
             cds = self.CDSdct[geneid]
             if cds.endswith("TGA") or cds.endswith("TAA") or cds.endswith("TAG"):
-                print(geneid + "\t" + "stop" + "yes")
+                dct[geneid] = "y"
             else:
-                print(geneid + "\t" + "stop" + "no")
+                dct[geneid] = "n" 
+        self.ifstopcodon = dct
 
     def check_3n(self):
+        dct = dict()
         for geneid in self.CDSdct.keys():
             cds = self.CDSdct[geneid]
             if len(cds) % 3 == 0:
-                print(geneid + "\t" + "3n" + "yes")
+                dct[geneid] = "y"
             else:
-                print(geneid + "\t" + "3n" + "no")
+                dct[geneid] = "n"
+        self.if3n = dct
+
+    def get_PEP_dct(self):
+        dct = dict()
+        for geneid in self.CDSdct.keys():
+            cds = self.CDSdct[geneid]
+            pep = str(Seq(cds).translate())
+            dct[geneid] = pep
+        self.PEPdct = dct
 
 if __name__ == '__main__':
     c = CDS()
+    write_dct_fasta(c.CDSdct, "data/CDS.fa")
+    write_dct_fasta(c.PEPdct, "data/PEP.fa")
+    write_dct_table(c.ifstopcodon, "data/ifstopcodon.txt")
+    write_dct_table(c.if3n, "data/if3n.txt")
